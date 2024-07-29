b68e75f14afe9e936f6cf7851d4cb71eaa941940
Rhynia <vssr@foxmail.com>
2024-07-29 16:47:10 +0800

    Tweak

---
+++ Source/HyperUnity/Comps/ThingComps/CompTempSet.cs
@@ -0,0 +1,50 @@
+﻿using Verse;
+
+namespace HyperUnity
+{
+  public class CompProperties_TemperatureControl : CompProperties
+  {
+    public CompProperties_TemperatureControl()
+    {
+      compClass = typeof(CompTemperatureControl);
+    }
+  }
+  public class CompTemperatureControl : ThingComp
+  {
+    private bool _activated;
+    private float _targetTemperature = 21f;
+    
+    public override void CompTick()
+    {
+      base.CompTick();
+      if (!_activated || !parent.Spawned)
+      {
+        return;
+      }
+      TweakTemperature();
+    }
+
+    public override void PostExposeData()
+    {
+      base.PostExposeData();
+      Scribe_Values.Look(ref _activated, "activated");
+    }
+
+    private void TweakTemperature()
+    {
+      var room = parent.GetRoom();
+      if (room == null)
+      {
+        this.ThrowMote("R_HyperUnity_CompThingGuardian_Mote1".Translate());
+        _activated = false;
+        return;
+      }
+
+      var present = room.Temperature;
+      if (present < _targetTemperature || present > _targetTemperature)
+      {
+        room.Temperature = _targetTemperature;
+      }
+    }
+  }
+}
---
+++ Source/HyperUnity/Comps/ThingComps/CompThingGuardian.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+
+namespace HyperUnity
+{
+  public class CompProperties_RepairMachine : CompProperties
+  {
+    public int checkInterval = 60;
+    public float radius = 3.0f;
+    
+    public CompProperties_RepairMachine()
+    {
+      compClass = typeof(CompRepairMachine);
+    }
+  }
+  
+  public class CompRepairMachine : ThingComp
+  {
+    private CompProperties_RepairMachine Props => (CompProperties_RepairMachine)props;
+
+    public override void CompTick()
+    {
+      base.CompTick();
+      if (!parent.IsHashIntervalTick(Props.checkInterval) || !parent.Spawned)
+      {
+        return;
+      }
+    }
+
+    private void DoRepair()
+    {
+      var grid = Utility.ThingGridInRange(parent.Position, parent.Map, Props.radius);
+    }
+  }
+}
